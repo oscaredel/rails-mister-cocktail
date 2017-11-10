c929317f78e94a37efeefab253b824018f6b0883
Oscar Edel <edeloscar@gmail.com>
2017-11-10 14:28:21 +0100

    Made the DoseController work

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -2,6 +2,7 @@ class DosesController < ApplicationController
   before_action :set_cocktail, only: [:new, :create]
 
   def new
+    @ingredients = Ingredient.all
     @dose = Dose.new
   end
 
@@ -11,6 +12,7 @@ class DosesController < ApplicationController
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
+      @ingriedients = Ingredient.all
       render :new
     end
   end
