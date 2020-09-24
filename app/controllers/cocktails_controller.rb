5d400ec4084cfb1077a38bdea3df89d1a90e9a10
Oscar Edel <edeloscar@gmail.com>
2020-09-24 17:50:50 +0200

    Add search to index

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,6 +1,10 @@
 class CocktailsController < ApplicationController
   def index
+    if params[:query].present?
+      @cocktails = Cocktail.where("name ILIKE?", "%#{params[:query]}%")
+    else
     @cocktails = Cocktail.all
+  end
   end
 
   def show
